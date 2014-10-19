2e996fd6cd5f31ea2bd8c067503f81dbfe7e7c1d
unknown <tiagomvieira@gmail.com>
2014-10-19 12:45:36 +0100

    Code cleanup.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
-import httplib, urllib,urllib2,socket
-from BeautifulSoup import BeautifulSoup
-from urllib2 import Request, build_opener, HTTPCookieProcessor, HTTPHandler
+import urllib
+import urllib2
+import socket
 import cookielib
-
+from BeautifulSoup import BeautifulSoup
 
 def monthNumber(monthName):
     return {
@@ -47,11 +47,11 @@ urllib2.install_opener(opener)
 First we do a get,scan the page and create de csv files for the first month
 '''
 document = open('data/obitos.csv', 'w')
-document.write('data,numObitosCausaNatural,numObitosCausaNaoNatural,numObitosSujeitoInvestigacao,numAcidenteTrabalho,numAcidenteTransito,numEventualSuicidio,numEventualHomicidio,numOutrosAcidentes,numIgnorado')
+document.write('data,numObitosCausaNatural,numObitosCausaNaoNatural,numObitosSujeitoInvestigacao,numAcidenteTrabalho,numAcidenteTransito,numEventualSuicidio,numEventualHomicidio,numOutrosAcidentes,numIgnorado\n')
 
 req = urllib2.Request('https://servicos.min-saude.pt/sico/faces/estatisticas.jsp')
-req.add_header('Accept-Encoding',"gzip, deflate")
-req.add_header('Accept-Language',"pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3")
+req.add_header('Accept-Encoding', 'gzip, deflate')
+req.add_header('Accept-Language', 'pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3')
 req.add_header('Content-type', 'application/x-www-form-urlencoded')
 req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
 req.add_header('User-agent', 'Mozilla/5.0')
@@ -66,23 +66,23 @@ def cyclicGet( response ):
     soup = BeautifulSoup(data)
     cookies = response.headers.get('Set-Cookie')
 
-    numObitosCausaNatural = soup.find('input', {'id':'numObitosCausaNatural'}).get('value')
-    numObitosCausaNaoNatural = soup.find('input', {'id':'numObitosCausaNaoNatural'}).get('value')
-    numObitosSujeitoInvestigacao = soup.find('input', {'id':'numObitosSujeitoInvestigacao'}).get('value')
-    numAcidenteTrabalho = soup.find('input', {'id':'numAcidenteTrabalho'}).get('value')
-    numAcidenteTransito = soup.find('input', {'id':'numAcidenteTransito'}).get('value')
-    numEventualSuicidio = soup.find('input', {'id':'numEventualSuicidio'}).get('value')
-    numEventualHomicidio = soup.find('input', {'id':'numEventualHomicidio'}).get('value')
-    numOutrosAcidentes = soup.find('input', {'id':'numOutrosAcidentes'}).get('value')
-    numIgnorado = soup.find('input', {'id':'numIgnorado'}).get('value')
-    id = soup.find('input', {'name':'oracle.adf.faces.STATE_TOKEN'}).get('value')
+    numObitosCausaNatural = soup.find('input', {'id': 'numObitosCausaNatural'}).get('value')
+    numObitosCausaNaoNatural = soup.find('input', {'id': 'numObitosCausaNaoNatural'}).get('value')
+    numObitosSujeitoInvestigacao = soup.find('input', {'id': 'numObitosSujeitoInvestigacao'}).get('value')
+    numAcidenteTrabalho = soup.find('input', {'id': 'numAcidenteTrabalho'}).get('value')
+    numAcidenteTransito = soup.find('input', {'id': 'numAcidenteTransito'}).get('value')
+    numEventualSuicidio = soup.find('input', {'id': 'numEventualSuicidio'}).get('value')
+    numEventualHomicidio = soup.find('input', {'id': 'numEventualHomicidio'}).get('value')
+    numOutrosAcidentes = soup.find('input', {'id': 'numOutrosAcidentes'}).get('value')
+    numIgnorado = soup.find('input', {'id': 'numIgnorado'}).get('value')
+    id = soup.find('input', {'name': 'oracle.adf.faces.STATE_TOKEN'}).get('value')
 
-    columnInfoMoth = soup.find('input', {'id':'columnInfoMoth'}).get('value')
-    columnInfoWeek = soup.find('input', {'id':'columnInfoWeek'}).get('value')
-    columnInfoTotal = soup.find('input', {'id':'columnInfoTotal'}).get('value')
-    obitosSemanaInfo = soup.find('input', {'id':'columnInfoTotal'}).get('value')
-    obitosDiarioInfo = soup.find('input', {'id':'obitosDiarioInfo'}).get('value')
-    month = soup.find('span', {'class':'tituloMes'}).text
+    columnInfoMoth = soup.find('input', {'id': 'columnInfoMoth'}).get('value')
+    columnInfoWeek = soup.find('input', {'id': 'columnInfoWeek'}).get('value')
+    columnInfoTotal = soup.find('input', {'id': 'columnInfoTotal'}).get('value')
+    obitosSemanaInfo = soup.find('input', {'id': 'columnInfoTotal'}).get('value')
+    obitosDiarioInfo = soup.find('input', {'id': 'obitosDiarioInfo'}).get('value')
+    month = soup.find('span', {'class': 'tituloMes'}).text
     # separar o mes do ano Outubro - 2014
     date = month.split()
 
@@ -98,11 +98,20 @@ def cyclicGet( response ):
 
     count = 0
     numeroDias = len(numObitosCausaNaturalArray)
-    for day in range(1,numeroDias+1):
-        if numObitosCausaNaturalArray[numeroDias-day]=='0':
-            count+=1
+    for day in range(1,numeroDias + 1):
+        if numObitosCausaNaturalArray[numeroDias-day] == '0':
+            count += 1
         else:
-            document.write(date[2]+"-"+monthNumber(date[0])+'-'+str(numeroDias+1-day)+','+numObitosCausaNaturalArray[numeroDias-day]+','+numObitosCausaNaoNaturalArray[numeroDias-day]+','+numObitosSujeitoInvestigacaoArray[numeroDias-day]+','+numAcidenteTrabalhoArray[numeroDias-day]+','+numAcidenteTransitoArray[numeroDias-day]+','+numEventualSuicidioArray[numeroDias-day]+','+numEventualHomicidioArray[numeroDias-day]+','+numOutrosAcidentesArray[numeroDias-day]+','+numIgnoradoArray[numeroDias-day]+'\n')
+            document.write(date[2] + "-" + monthNumber(date[0]) + '-' + str(numeroDias + 1 - day) + ','
+                 + numObitosCausaNaturalArray[numeroDias-day] + ','
+                 + numObitosCausaNaoNaturalArray[numeroDias-day] + ','
+                 + numObitosSujeitoInvestigacaoArray[numeroDias-day] + ','
+                 + numAcidenteTrabalhoArray[numeroDias-day] + ','
+                 + numAcidenteTransitoArray[numeroDias-day] + ','
+                 + numEventualSuicidioArray[numeroDias-day] + ','
+                 + numEventualHomicidioArray[numeroDias-day] + ','
+                 + numOutrosAcidentesArray[numeroDias-day] + ','
+                 + numIgnoradoArray[numeroDias-day] + '\n')
 
     if count == numeroDias:
         return
@@ -113,13 +122,34 @@ def cyclicGet( response ):
     The post returns a redirect which we don't follow. Instead we grab the cookies and do another get.
     '''
 
-    values = {'oracle.adf.faces.STATE_TOKEN':id,'source':'_id0:anterior','oracle.adf.faces.FORM':'_id0','event':'update','selectedRow':'4','_id0:tabelaVersoes:selected':'4','_id0:tabelaVersoes:rangeStart':'0','numObitosCausaNatural':'','columnInfoMoth': columnInfoMoth.replace(" ", "+"),'columnInfoWeek': columnInfoWeek.replace(" ", "+")+'-','columnInfoTotal': columnInfoTotal.replace(" ", "+")+'-','numObitosCausaNatural': numObitosCausaNatural.replace(" ", "+"),'numObitosCausaNaoNatural': numObitosCausaNatural.replace(" ", "+"),'numObitosSujeitoInvestigacao': numObitosSujeitoInvestigacao.replace(" ", "+"),'numAcidenteTrabalho': numAcidenteTrabalho.replace(" ", "+"),'numAcidenteTransito': numAcidenteTransito.replace(" ", "+"),'numEventualSuicidio': numEventualSuicidio.replace(" ", "+"),'numEventualHomicidio': numEventualHomicidio.replace(" ", "+"),'numOutrosAcidentes': numOutrosAcidentes.replace(" ", "+"),'numIgnorado': numIgnorado.replace(" ", "+"),'obitosSemanaInfo': obitosSemanaInfo.replace(" ", "+"),'obitosDiarioInfo':obitosDiarioInfo.replace(" ", "+")+'-'}
+    values = {
+    'oracle.adf.faces.STATE_TOKEN':id,
+    'source':'_id0:anterior',
+    'oracle.adf.faces.FORM':'_id0',
+    'event':'update','selectedRow':'4',
+    '_id0:tabelaVersoes:selected':'4',
+    '_id0:tabelaVersoes:rangeStart':'0',
+    'numObitosCausaNatural':'',
+    'columnInfoMoth': columnInfoMoth.replace(" ", "+"),
+    'columnInfoWeek': columnInfoWeek.replace(" ", "+") + '-',
+    'columnInfoTotal': columnInfoTotal.replace(" ", "+") + '-',
+    'numObitosCausaNatural': numObitosCausaNatural.replace(" ", "+"),
+    'numObitosCausaNaoNatural': numObitosCausaNatural.replace(" ", "+"),
+    'numObitosSujeitoInvestigacao': numObitosSujeitoInvestigacao.replace(" ", "+"),
+    'numAcidenteTrabalho': numAcidenteTrabalho.replace(" ", "+"),
+    'numAcidenteTransito': numAcidenteTransito.replace(" ", "+"),
+    'numEventualSuicidio': numEventualSuicidio.replace(" ", "+"),
+    'numEventualHomicidio': numEventualHomicidio.replace(" ", "+"),
+    'numOutrosAcidentes': numOutrosAcidentes.replace(" ", "+"),
+    'numIgnorado': numIgnorado.replace(" ", "+"),
+    'obitosSemanaInfo': obitosSemanaInfo.replace(" ", "+"),
+    'obitosDiarioInfo':obitosDiarioInfo.replace(" ", "+") + '-'}
 
     data = urllib.urlencode(values)
     req = urllib2.Request('https://servicos.min-saude.pt/sico/faces/estatisticas.jsp', data)
     req.add_header('Cookie', cookies)
-    req.add_header('Accept-Encoding',"gzip, deflate")
-    req.add_header('Accept-Language',"pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3")
+    req.add_header('Accept-Encoding','gzip, deflate')
+    req.add_header('Accept-Language','pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3')
     req.add_header('Content-type', 'application/x-www-form-urlencoded')
     req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
     req.add_header('User-agent', 'Mozilla/5.0')
@@ -134,8 +164,8 @@ def cyclicGet( response ):
 # ------------------------- get -----------------------
     req = urllib2.Request('https://servicos.min-saude.pt/sico/faces/estatisticas.jsp')
     req.add_header('Cookie', cookies)
-    req.add_header('Accept-Encoding',"gzip, deflate")
-    req.add_header('Accept-Language',"pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3")
+    req.add_header('Accept-Encoding','gzip, deflate')
+    req.add_header('Accept-Language','pt-pt,pt;q=0.8,en;q=0.5,en-us;q=0.3')
     req.add_header('Content-type', 'application/x-www-form-urlencoded')
     req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
     req.add_header('User-agent', 'Mozilla/5.0')
@@ -147,11 +177,11 @@ def cyclicGet( response ):
 
     return response
 
-i=1
+i = 1
 while True:
-    print "iteration",i
+    print "iteration", i
     response = cyclicGet(response)
-    i+=1
+    i += 1
     if not response:
         break
 
